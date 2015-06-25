270bf0023ef9bc3bb9d730aa99e6e381e54c15e2
Yhoon Kim <yhoonkim01@gmail.com>
2015-06-25 12:15:09 +0900

    feat: remember me (closes #6)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,38 +4,39 @@ class SessionsController < ApplicationController
     # raise user_signed_in?.inspect
     if user_signed_in?
       redirect_to report_path
+    elsif !cookies.permanent[:access_token].blank?
+      auth = Authorization.find_by_access_token(cookies.permanent[:access_token])
+      session[:user_id] = auth.user.id
+      redirect_to report_path
     end
   end
 
   def create
+
     auth_hash = request.env['omniauth.auth']
 
     if session[:user_id]
       # Means our user is signed in. Add the authorization to the user
       User.find(session[:user_id]).add_provider(auth_hash)
-
       render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
     else
-
-
       access_token = WunderlistApi.get_access_token(params[:code])
-
-      #Todo
-      #1) access_token 받아오기
-      #2) curl 보내는 함수 만들기
-
 
       # Log him in or sign him up
       auth = Authorization.find_or_create(auth_hash)
       auth.update(access_token: access_token)
       auth.save
 
+      cookies.permanent[:access_token] = access_token
+
+
       # Create the session
       session[:user_id] = auth.user.id
-
-      redirect_to report_path
-      # render :text => "Welcome #{auth.user.name}!"
     end
+
+    redirect_to report_path
+    # render :text => "Welcome #{auth.user.name}!"
+
   end
 
   def failure
@@ -44,7 +45,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    render :text => "You've logged out!"
+    cookies.permanent[:access_token] = nil
+    redirect_to root_path
   end
 
 end
