82e144021d660286f4242c54e6b365ee83fc63c3
Yhoon Kim <yhoonkim01@gmail.com>
2015-07-15 19:43:20 +0900

    feat: avg label (#22)

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -19,12 +19,20 @@ $(document).on('ready page:load', function () {
                        .domain([0, todoDone.average, todoDone.average * 2])
                        .range([10, height / 4 * 0.9, height / 4 * 0.9]);
 
-  function avgCircleClass(){
+  function avgLabelX(avg, today){
+    if( avg >= today )
+      return height/2 + rScale(todoDone.average) + 16;
+    else
+      return height/2 + rScale(todoDone.average) - 16;
+
+  }
+
+  function invertClass(className){
     if( todoDone.average > todoDone.today ){
-      return "averageCircle";
+      return className;
     }
     else
-      return "averageCircle invert"
+      return className + " invert"
   }
 
   var svg = d3.select("div#reportVis")
@@ -52,11 +60,21 @@ $(document).on('ready page:load', function () {
                               .text(todoDone.today)
 
   var averageCircle = circles.append("circle")
-                          .attr("class", avgCircleClass() )
+                          .attr("class", invertClass("averageCircle") )
                           .attr("cx", width/2 )
                           .attr("cy", height/2 )
                           .attr("r", rScale(todoDone.average) )
                           .attr("stroke-dasharray", "10, 10")
+
+  var avgPointText = circles.append("text")
+                              .attr("class", invertClass("avgPointText") )
+                              .attr("x", width/2 )
+                              .attr("y", avgLabelX(todoDone.average, todoDone.today))
+                              .attr("font-size", "10pt")
+                              .attr("dy", ".35em")
+                              .attr("opacity", 0.7)
+                              .attr("text-anchor", "middle")
+                              .text("avg : " + todoDone.average)
 
 
 
