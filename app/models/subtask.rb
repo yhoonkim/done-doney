371f3e67629d3eb2123b317e4263410657a7911a
Yhoon Kim <yhoonkim01@gmail.com>
2015-07-31 12:58:27 +0900

    feat: subtask should be sorted by title

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,6 +1,10 @@
 class Subtask < ActiveRecord::Base
   belongs_to :task
 
+  scope :order_by_title_without_point, -> {
+    all.sort{ |a, b| a.title_without_point <=> b.title_without_point }
+  }
+
   def self.create_by_wunderlist(raw_subtask)
     create(id: raw_subtask[:id],
           original_created_at: raw_subtask[:created_at],
@@ -52,6 +56,15 @@ def self.get_point(title)
     regex = /^\[([0-9*]+)\](.*)$/
     if matching = regex.match(title)
       "[#{new_point}]" + matching[2]
+    else
+      title
+    end
+  end
+
+  def title_without_point
+    regex = /^\[([0-9*]+)\](.*)$/
+    if matching = regex.match(title)
+      matching[2]
     else
       title
     end
